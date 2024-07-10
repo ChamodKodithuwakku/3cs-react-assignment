c1d269d0138c078c459b5902da291ee82031f2cf
chamod <ckodi2000@gmail.com>
2024-07-10 14:47:29 +0530

    added datafecher.js file to fech json data

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import DataFetcher from './DataFetcher';
 
 const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <DataFetcher />
     </div>
   );
 };
